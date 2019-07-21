f394933e4600a06e51c8e11ce15b0bec6353c2f5
Sarah Witt <witt.s@husky.neu.edu>
2019-07-21 11:23:05 -0400

    Updates

--- src/styles/GlobalStyle.js
+++
@@ -1,8 +0,0 @@
-import { createGlobalStyle } from "styled-components"
-
-export default createGlobalStyle`
-  body {
-    background-color: #6fc7ab;
-    
-  }
-`
---
+++ styles-6560e460332e246283ef.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{169:function(n,w,o){}}]);
+//# sourceMappingURL=styles-6560e460332e246283ef.js.map
